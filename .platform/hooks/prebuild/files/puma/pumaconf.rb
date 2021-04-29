06edea514c288c0938c834372ea9a0d2a02c6afc
Shai Coleman <shai.coleman@cashanalytics.com>
2021-04-29 19:17:07 +0100

    Custom puma configuration

--- .platform/hooks/prebuild/files/puma/pumaconf.rb
+++ .platform/hooks/prebuild/files/puma/pumaconf.rb
@@ -1,5 +1,9 @@
 directory '/var/app/current'
-threads 8, 32
-workers %x(grep -c processor /proc/cpuinfo)
+workers_count = Integer(ENV.fetch("WEB_CONCURRENCY") { 2 })
+threads_count = Integer(ENV.fetch("RAILS_MAX_THREADS") { 5 })
+workers workers_count
+threads threads_count, threads_count
+preload_app! if workers_count > 0
 bind 'unix:///var/run/puma/my_app.sock'
 stdout_redirect '/var/log/puma/puma.log', '/var/log/puma/puma.log', true
+daemonize false
