ee4a93dae2d0001e95a6d954442f8c416e4ccdeb
Shai Coleman <shai.coleman@cashanalytics.com>
2021-04-30 22:10:04 +0100

    Upgrade to puma 5

--- .platform/hooks/prebuild/files/puma/pumaconf.rb
+++ .platform/hooks/prebuild/files/puma/pumaconf.rb
@@ -6,4 +6,3 @@ threads threads_count, threads_count
 preload_app! if workers_count > 0
 bind 'unix:///var/run/puma/my_app.sock'
 stdout_redirect '/var/log/puma/puma.log', '/var/log/puma/puma.log', true
-daemonize false
