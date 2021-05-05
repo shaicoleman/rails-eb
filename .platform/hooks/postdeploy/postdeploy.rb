e708f30286052229e4cd58a2917a005c4bd30af1
Shai Coleman <shai.coleman@cashanalytics.com>
2021-05-05 19:56:13 +0100

    Update motd after deploy

---
+++ .platform/hooks/postdeploy/postdeploy.rb
@@ -0,0 +1,30 @@
+#!/opt/elasticbeanstalk/lib/ruby/bin/ruby
+# frozen_string_literal: true
+
+require 'open3'
+
+def main
+  update_motd
+end
+
+def update_motd
+  run('update-motd')
+end
+
+def run(cmd, ignore_errors: false)
+  log("Run: #{cmd}")
+  stdout_str, stderr_str, status = Open3.capture3(cmd)
+  unless status.success?
+    message = "Error running: #{cmd}\nOutput: #{stdout_str}, Errors: #{stderr_str}"
+    log(message)
+    abort(message) unless ignore_errors
+  end
+  { stdout: stdout_str, stderr: stderr_str, status: status }
+end
+
+def log(message)
+  puts message
+  File.open('/var/log/prebuild.log', 'a') { |f| f.print "#{Time.now.utc} #{message}\n" }
+end
+
+main
