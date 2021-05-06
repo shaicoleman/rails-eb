9ea15a810f20536269b7fbff089bd58cc9972697
Shai Coleman <shai.coleman@cashanalytics.com>
2021-05-06 12:36:57 +0100

    Update prompt style

--- .platform/hooks/prebuild/files/profile.d/prompt.sh
+++ .platform/hooks/prebuild/files/profile.d/prompt.sh
@@ -1,3 +1,3 @@
 EB_ENV="$(/opt/elasticbeanstalk/bin/get-config container --key 'environment_name')"
 EB_ENV_SHORT="${EB_ENV/rails-eb-/eb-}"
-PS1="[\u@${EB_ENV_SHORT} \W]\\$ "
+PS1="\u@${EB_ENV_SHORT}:\w\\$ "
