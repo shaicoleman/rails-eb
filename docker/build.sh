7b53c6935bfe54db14d78e37c4f910b99c032a5e
Shai Coleman <shai.coleman@cashanalytics.com>
2021-05-07 11:19:35 +0100

    Docker build scripts changes to script directory

--- docker/build.sh
+++ docker/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+cd "$(dirname "$0")"
 source config/env.sh
 DOCKER_BUILDKIT=1 \
   docker build --progress=plain --rm --build-arg RAILS_ENV="$RAILS_ENV" --build-arg NODE_ENV="$NODE_ENV" --file Dockerfile -t al2 .. || exit 1
