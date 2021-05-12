f5ad4df13be6705d56b748102dee029141271097
Shai Coleman <shai.coleman@cashanalytics.com>
2021-05-12 20:44:15 +0100

    Show instance_id

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,8 @@
 class HomeController < ApplicationController
   def index
-    hostname = "#{`hostname`.chomp}"
+    instance_id = `ec2-metadata --instance-id || true`.chomp.presence || `hostname`.chomp
     build_info = File.read('./.build/build-info.txt').scan(/^([^:]+): (.*)$/).to_h
     now = "#{Time.now.utc}"
-    render json: { now: now, hostname: hostname, build_info: build_info }
+    render json: { now: now, instance_id: instance_id, build_info: build_info }
   end
 end
