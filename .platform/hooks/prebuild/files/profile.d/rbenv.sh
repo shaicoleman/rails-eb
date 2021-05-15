7965717caa37a9f365fbfdd5991820e1a1809abb
Shai Coleman <shai.coleman@cashanalytics.com>
2021-05-15 13:42:14 +0100

    Do not initialize rbenv

---
+++ .platform/hooks/prebuild/files/profile.d/rbenv.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# rbenv setup
+# Make sure we keep RBENV_ROOT up to date with current rbenv
+# install directory.
+export RBENV_ROOT=/opt/elasticbeanstalk/.rbenv
+# export PATH="$RBENV_ROOT/bin:$PATH"
+# eval "$(rbenv init -)"
