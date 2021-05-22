f8e23a0eafbdac1c2111081939862ae02edc32f9
Shai Coleman <shai.coleman@cashanalytics.com>
2021-05-22 19:01:57 +0100

    Allow webapp user access to environment variable in shell

--- .platform/hooks/postdeploy/postdeploy.rb
+++ .platform/hooks/postdeploy/postdeploy.rb
@@ -5,8 +5,13 @@ require_relative '../prebuild/build_utils'
 
 def main
   init
+  chown_env_file_to_webapp
   update_motd
   finish  
+end
+
+def chown_env_file_to_webapp
+  FileUtils.chown 'webapp', 'webapp', '/opt/elasticbeanstalk/deployment/env'
 end
 
 def update_motd
--- .platform/hooks/prebuild/files/profile.d/profile.sh
+++ .platform/hooks/prebuild/files/profile.d/profile.sh
@@ -1,4 +1,7 @@
-source /opt/elasticbeanstalk/deployment/env
+set -o allexport
+[[ -r /opt/elasticbeanstalk/deployment/env ]] && source /opt/elasticbeanstalk/deployment/env
+set +o allexport
+
 export PATH="$HOME/.local/bin:$HOME/bin:/opt/elasticbeanstalk/.rbenv/shims:/opt/elasticbeanstalk/.rbenv/bin:/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin"
 
 alias l='ls -Al'
