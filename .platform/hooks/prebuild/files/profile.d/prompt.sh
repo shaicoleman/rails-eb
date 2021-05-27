099e1d4244e4eb380ca74a94de14c108631d820d
Shai Coleman <shai.coleman@cashanalytics.com>
2021-05-27 14:05:28 +0100

    Update prompt

--- .platform/hooks/prebuild/files/profile.d/prompt.sh
+++ .platform/hooks/prebuild/files/profile.d/prompt.sh
@@ -2,4 +2,4 @@ eb_env="$(/opt/elasticbeanstalk/bin/get-config container --key 'environment_name
 instance_id="$(ec2-metadata --instance-id | grep -oP 'instance-id: \K(i-.*)')"
 short_eb_env="${eb_env/rails-eb-/eb-}"
 short_instance_id="${instance_id:2:4}"
-PS1="[\u@${short_eb_env}-${short_instance_id} \w]\\$ "
+PS1="[\u@${short_eb_env}:${short_instance_id} \w]\\$ "
