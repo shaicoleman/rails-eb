570a33a2825fa38b3e3da9dff93d9e014a9bbf77
Shai Coleman <shai.coleman@cashanalytics.com>
2021-05-27 14:07:22 +0100

    Support deployment to a different environment

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@
 export AWS_PROFILE=cashanalytics-development
 ./build.rb &&
 . ~/bin/aws-mfa &&
-\eb status
-/usr/bin/time --format='%e sec' eb deploy --timeout 60
+\eb status $1
+/usr/bin/time --format='%e sec' eb deploy --timeout 60 $1
